f2c0196ed0991849579174249e59afa8e8e2e2ab
이창훈 <73262261+leechanghoon3024@users.noreply.github.com>
2025-02-24 02:08:50 +0900

    Feat: #4 노션 페이지 추가 (임베디드 옵션인데 배포확인)

---
+++ src/components/iframe/iframe.notion.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export const IframeNotion = () => {
+    return (
+        <iframe
+            src="https://dolomite-smartphone-f70.notion.site/ebd/18f02b54c9408059b7d2f0d83debd53b"
+            style={{ width: '100%', height: '80vh' }}
+            allowFullScreen
+        />
+    );
+};
