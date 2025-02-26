873df613736ade4c26c30013e8d8475daf6906bb
이창훈 <73262261+leechanghoon3024@users.noreply.github.com>
2025-02-26 18:36:26 +0900

    Fix: #13 api url vercel 2

--- src/const/api.const.ts
+++ src/const/api.const.ts
@@ -1,8 +1,4 @@
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || '';
 export const getBaseUrl = () => {
-    if (process.env.NODE_ENV === 'development') {
-        return `${process.env.NEXT_PUBLIC_API_URL}`;
-    }
-    console.log('NEXT_PUBLIC_VERCEL_URL', process.env.NEXT_PUBLIC_VERCEL_URL);
-    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+    return `${process.env.NEXT_PUBLIC_API_URL}`;
 };
