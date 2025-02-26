bebe77129f305f369daad6405115b1438099a4a4
이창훈 <73262261+leechanghoon3024@users.noreply.github.com>
2025-02-26 11:45:59 +0900

    add. @vercel/analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,6 +56,7 @@ export default function RootLayout({
                 <MainHeader />
                 <BackgroundWrapper />
                 {children}
+                <Analytics />
             </body>
         </html>
     );
